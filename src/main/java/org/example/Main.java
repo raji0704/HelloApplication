b20c99f6a8b30a5ee22f1754058314430b88f355
raji0704 <akularajeswari120@gmail.com>
2026-03-23 20:18:03 +0530

    added uc7 code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,14 +5,7 @@ public class Main {
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            String names = "";
-
-            for (String name : args) {
-                names += name + ", ";
-            }
-
-            names = names.substring(0, names.length() - 2);
-
+            String names = String.join(", ", args);
             System.out.println("Hello, " + names + "!");
         }
     }

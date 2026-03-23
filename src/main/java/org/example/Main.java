5514f5c8d431a5601ce7677f5548e6ffe4c1c069
raji0704 <akularajeswari120@gmail.com>
2026-03-23 19:04:35 +0530

    added uc3 code

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println("Hello " + args[0]);
+        } else {
+            System.out.println("Please provide a name");
+        }
+    }
+}

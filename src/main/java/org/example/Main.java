13c18ee8eb270df22cef532e07b5722151a187f2
raji0704 <akularajeswari120@gmail.com>
2026-03-23 20:06:41 +0530

    added uc6 code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,15 +3,17 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("Hello, Bhargavvv !");
+            System.out.println("Hello, World!");
         } else {
-            StringBuilder names = new StringBuilder();
+            String names = "";
 
             for (String name : args) {
-                names.append(name).append(" ");
+                names += name + ", ";
             }
 
-            System.out.println("Hello, " + names.toString().trim() + "!");
+            names = names.substring(0, names.length() - 2);
+
+            System.out.println("Hello, " + names + "!");
         }
     }
 }
